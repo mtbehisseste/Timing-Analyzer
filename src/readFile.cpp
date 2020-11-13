31e7b897b797b392d95f2d45432bd9a5c0e03bcc
mtbehisseste <monkey860315@gmail.com>
2020-11-13 14:39:34 +0800

    Use sscanf() to simplify parsing process

--- src/readFile.cpp
+++ src/readFile.cpp
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <cstdio>
 #include <cstring>
 
 #include "readFile.h"
@@ -16,48 +17,33 @@ void readLibrary(Node *&libHead, string libName)
     }
 
     char line[1000];
+    string strline;
     size_t first_quote, last_quote;
+
     // skip unit attributes
     for (int i = 0; i < 9; ++i)
         fLib.getline(line, 1000);
 
     // record output net capacitances and input transition time
     float outputCapacitance[7], inputTransitionTime[7];
-    string strline, subline, tok;
-    int cntTok = 0, commaIndex = 0;
 
     fLib.getline(line, 1000);  // index 1
     strline = string(line);
     first_quote = strline.find_first_of("\"");
     last_quote = strline.find_last_of("\"");
-    subline = strline.substr(first_quote + 1, last_quote-first_quote - 1);
-    commaIndex = subline.find(",");
-    tok = subline.substr(0, commaIndex);
-    subline = subline.substr(commaIndex + 1);
-    while (commaIndex <= subline.length()) {
-        outputCapacitance[cntTok++] = stof(tok);
-        commaIndex = subline.find(",");
-        tok = subline.substr(0, commaIndex);
-        subline = subline.substr(commaIndex + 1);
-    }
-    outputCapacitance[cntTok] = stof(tok);
+    strline = strline.substr(first_quote + 1, last_quote-first_quote - 1);
+    sscanf(strline.c_str(), "%f,%f,%f,%f,%f,%f,%f", &outputCapacitance[0],
+            &outputCapacitance[1], &outputCapacitance[2], &outputCapacitance[3],
+            &outputCapacitance[4], &outputCapacitance[5], &outputCapacitance[6]);
 
-    cntTok = 0;
     memset(line, 0, 1000);
-    fLib.getline(line, 1000);  // index 1
+    fLib.getline(line, 1000);  // index 2
     strline = string(line);
     first_quote = strline.find_first_of("\"");
     last_quote = strline.find_last_of("\"");
-    subline = strline.substr(first_quote + 1, last_quote-first_quote - 1);
-    commaIndex = subline.find(",");
-    tok = subline.substr(0, commaIndex);
-    subline = subline.substr(commaIndex + 1);
-    while (commaIndex <= subline.length()) {
-        inputTransitionTime[cntTok++] = stof(tok);
-        commaIndex = subline.find(",");
-        tok = subline.substr(0, commaIndex);
-        subline = subline.substr(commaIndex + 1);
-    }
-    inputTransitionTime[cntTok] = stof(tok);
+    strline = strline.substr(first_quote + 1, last_quote-first_quote - 1);
+    sscanf(strline.c_str(), "%f,%f,%f,%f,%f,%f,%f", &inputTransitionTime[0],
+            &inputTransitionTime[1], &inputTransitionTime[2], &inputTransitionTime[3],
+            &inputTransitionTime[4], &inputTransitionTime[5], &inputTransitionTime[6]);
 
 }
